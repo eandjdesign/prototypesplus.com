edbf0305d6f26044e74e4581fb42a3c122be41c0
fwg001 <james@eandjdesign.com>
2013-04-10 09:33:57 -0700

    GA script update, linked logo

--- inc/parts/header.php
+++ inc/parts/header.php
@@ -1,26 +1,32 @@
 <?php /* Site Header, Navigation, Logo, etc */ ?>
 
-		<script>
-			// Google Analytics Tracking
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-39524099-1', 'starlightcaregivers.com');
-			ga('send', 'pageview');
-
+		<script type="text/javascript">
+		
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-3787069-12']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
 		</script>
 
 		<header id="main-header" class="main-header row">
 			<div class="col-twelve">
 				<h1 class="aj-logo site-logo">
 					A+J Product Solutions
-					<img src="/assets/img/aj-logo.png" alt="Website Name" class="hide">
+					<a href="http://ajproductsolutions.com/"><img src="/assets/img/aj-logo.png" alt="Website Name" class="hide"></a>
 				</h1>
 				<h2 class="prototypesplus-logo site-logo">
 					Prototypes Plus
 					<img src="/assets/img/prototypesplus-logo.png" alt="Website Name" class="hide">
 				</h2>
 				
-				<h3>
+				<h1>
 					<span class="highlight">News flash!</span>
-					<br><strong>Prototypes Plus</strong> has joined <strong>A+J Product Solutions</strong></h3>
+					<br><strong>Prototypes Plus</strong> has joined <strong>A+J Product Solutions</strong></h1>
 			</div>
 		</header>
